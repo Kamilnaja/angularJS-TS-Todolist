69b55d4c5059439269cc4b210662dfb7bd62347c
= <kamilnaja@gmail.com>
2017-12-09 13:54:18 +0100

    you can add new task

---
+++ src/taskList.ts
@@ -0,0 +1,15 @@
+// export const TASKLIST = [
+//     {
+//         title: "lorem",
+//         id: 1,
+//         isDone: false
+//     }, {
+//         title: "ipsum",
+//         id: 2,
+//         isDone: true
+//     }, {
+//         title: "dolor",
+//         id: 3,
+//         isDone: false
+//     }
+// ];
